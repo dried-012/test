af0f129446031404a330b8a9b084b9a66f75579e
jaemin1020 <dried012@gmail.com>
2024-10-02 20:49:57 +0900

    a

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import './App.css';
+import React from 'react';
+import { db } from './firebase';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+
+function App() {
+  const [test, setTest] = useState();
+  async function getTest() {
+    const docRef = doc(db,"item","1");
+    try{
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setTest(docSnap.data());
+      } else {
+        console.error("No such Document");
+      }
+    } catch (error) {
+      console.error("Error getting doc",error)
+    }
+ 
+    
+  }
+  const handleClick = (e) => {
+    //console.log(e.target);
+    //console.log(e.target.value);
+    const answer = e.target.value;
+    if(answer == "4학년"){
+      alert("");
+    }else{
+      alert("");
+    }
+  };
+
+  //최초 마운트 시 getTest import
+  useEffect(() => {
+<<<<<<< HEAD
+    try{
+      console.log(db);
+      getTest();
+    } catch (error){
+      console.error(error);
+=======
+    console.log(db);
+    try {
+      getTest()
+    } catch (error) {
+      console.log(error);
+>>>>>>> cd440b28d2fbec8b132a2c05fd436b24c09e9602
+    }
+    
+  }, [])
+
+  return (
+    <div className='container'>
+      <div className='app'>
+        <div className='header'>
+          <span>
+            <h1>quiz test</h1>
+          </span>
+        </div>
+        <div className='content'>
+          <p><span> </span></p>{/*몇번 문제 출력*/}
+          <h1> </h1> {/*문제 내용 출력*/}
+          <div> {/*db 불러옴*/}
+            {test !== undefined &&
+            <div>{test.name}</div>}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
