11c5a5d223cf578c33b24785414ff6f9ec9a9bb6
dried-012 <dried012@gmail.com>
2024-10-29 13:57:56 +0900

    aa

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
-import {store} from "./redux/store";
+import { store } from "./redux/store";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/lib/integration/react";
 
