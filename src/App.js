83c90523216a7d062095d3f5d268cee62079699f
dried-012 <dried012@gmail.com>
2024-11-12 15:40:42 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import './css/Board.css';
 import React from 'react';
-import MyPage from './NavigateBar/Board';
+import MyPage from './NavigateBar/Mypage';
 import Board from './NavigateBar/Board';
 import MyTest from './NavigateBar/Test';
 import MainPage from "./mainPage";
-import { _apiKey } from './firebase';
 import { Routes, Route } from 'react-router-dom'
 
 import './TestContent.css'
