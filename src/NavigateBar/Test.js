d44b349170240a07743f8093cb7126bb3cbc6741
DEzyxw <rhgh0608@naver.com>
2024-11-14 12:30:00 +0900

    tempTest

--- src/NavigateBar/Test.js
+++ src/NavigateBar/Test.js
@@ -45,6 +45,7 @@ function Test(){
           if (docSnap.exists()) {
             const data = docSnap.data();
             setSelectedDocContent(data); // 전체 데이터를 상태에 저장
+            setAnswerVisible({});
           } else {
             console.error("Document does not exist!");
           }
@@ -54,61 +55,119 @@ function Test(){
       };
 
     return(
-        <div>
-            <div>
-                <div>
-                    <button onClick={testListButtonClick} value="eIP_wT" data-title="정보처리기사 필기">
-                        정보처리기사 필기
-                    </button>
-                    <button onClick={testListButtonClick} value="eIP_pT" data-title="정보처리기사 실기">
-                        정보처리기사 실기
-                    </button>
-                </div>
-                <div>
-                {testList.map((docName, index) => (
-                    <button key={index} onClick={() => handleDocClick(docName)} value={docName}>
-                    {formatDocName(docName)}
-                    </button>
-                ))}
-                </div>
-                <div>
-                {selectedDocContent && (
-                    <div>
-                    {Object.keys(selectedDocContent).map((key) => {
-                        const field = selectedDocContent[key];
-                        const isVisible = answerVisible[key]; // 각 문제별 정답 표시 상태
+      
+      <div>
 
-                        return (
-                        <div key={key} className="question-block">
-                            <div><h2>문제 {field.num}</h2></div>
-                            <div>
-                            <p>제목: {field.title}</p>
-                            <p>설명: {field.description}</p>
-                            </div>
-                            <div><p>입력: <textarea></textarea></p></div>
-                            <div>
+        <div>
+
+          <div id="testMainDiv">
+            <div id="testMainDivInside" className="Inside">
+
+
+              <div id="testMainListDiv">
+                <div id="testMainListDivInside" className="Inside">
+                  <div id="testMainListButtonDiv" className="TestWidth">
+                    <div id="testMainListButtonDivInside" className="Inside">
+                      <div>
+                        <button onClick={testListButtonClick} value="eIP_wT" data-title="정보처리기사 필기">
+                            정보처리기사 필기
+                        </button>
+                        <button onClick={testListButtonClick} value="eIP_pT" data-title="정보처리기사 실기">
+                            정보처리기사 실기
+                        </button>
+                      </div>
+                      <div>
+                      {testList.map((docName, index) => (
+                          <button key={index} onClick={() => handleDocClick(docName)} value={docName}>
+                          {formatDocName(docName)}
+                          </button>
+                      ))}
+                      </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="testMainTitleDiv">
+                <div id="testMainTitleDivInside" className="Inside">
+                  <div id="testMainTitleOnDiv" className="TestWidth">
+                    <div id="testMainTitleOnDivInside" className="Inside">
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="testMainSubjectDiv">
+                <div id="testMainSubjectDivInside" className="Inside">
+                  <div id="testMainSubjectSetDiv" className="TestWidth">
+                    <div id="testMainSubjectSetDivInside" className="Inside">
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="testMainContentDiv">
+                <div id="testMainContentDivInside" className="Inside">
+                  <div id="testMainContentQuestionDiv" className="TestWidth">
+                    <div id="testMainContentQuestionDivInside" className="Inside">
+
+                    <div id="testMainContentQuestionUpdateDiv">
+                    {selectedDocContent && (
+                        <div id="question-block-pack">
+                        {Object.keys(selectedDocContent).map((key) => {
+                            const field = selectedDocContent[key];
+                            const isVisible = answerVisible[key]; // 각 문제별 정답 표시 상태
+
+                            return (
+                              <div key={key} className="question-block">
+                                
+                                
+                                <p><span className="testNum">{field.num}</span> {field.title}</p>
+                                <p>{field.description}</p>
+                                
+                                <div><p><textarea className="testInsert" placeholder="답 입력"></textarea></p></div>
                                 <div>
-                                    <div onClick={() => toggleAnswer(key)}>
-                                    {!isVisible && (
-                                        <span className="AnswerClicker">정답 및 해설 보기 (클릭)</span>
-                                    )}
-                                    {isVisible && (
-                                        <div>
-                                        <p>정답: {field.answer}</p>
-                                        <p>해설: {field.explanation}</p>
+                                    <div>
+                                        <div onClick={() => toggleAnswer(key)}>
+                                        {!isVisible && (
+                                            <div>
+                                            <p><span className="AnswerClicker">정답 및 해설 보기 (클릭)</span></p>
+                                            <p><br></br></p>
+                                            </div>
+                                        )}
+                                        {isVisible && (
+                                            <div>
+                                            <p>정답: {field.answer}</p>
+                                            <p>해설: {field.explanation}</p>
+                                            </div>
+                                        )}
                                         </div>
-                                    )}
                                     </div>
                                 </div>
+                                <div>
+                                  <form>맞췄다면<input type="radio" name="OX" value="O"></input>틀렸다면<input type="radio" name="OX" value="X"></input></form>
+                                </div>
                             </div>
+                          );
+                         })}
                         </div>
-                        );
-                    })}
+                        )}
+                      </div>
+
                     </div>
-                )}
+                  </div>
                 </div>
+              </div>
+
+
             </div>
+          </div>
+
         </div>
+
+      </div>
     );
 }
 
